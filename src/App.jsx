ffa91c24567e48a0142ba8b37ed4660210213e0b
Pranav Jha <pranav.2023ug2022@iiitranchi.ac.in>
2026-02-05 23:23:50 +0530

    Welcome Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Challenge from "./components/Layouts/Challenge"
+import Dashboard from "./components/Layouts/Dashboard"
+import Layout from "./components/Layouts/Layout"
+import Welcome from "./components/Layouts/Welcome"
+
 function App() {
   
   return (
-    <>
-      
-    </>
+    
+    <Layout>
+      <Welcome></Welcome>
+      <Dashboard></Dashboard>
+      <Challenge></Challenge>
+    </Layout>
+    
   )
 }
 
--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -1,5 +1,22 @@
-export default function Layout(){
+export default function Layout(props){
+    const {children}= props
     return(
-        <div></div>
+        <>
+            <header>
+                <h1 className="text-gradient">Diction</h1>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <small>Created By</small>
+                <a target="_blank" href="www.linkedin.com/in/pranav-jha-135b432a5">
+                    <img alt="pfp" src="https://i.guim.co.uk/img/media/f76e2c99e1d0683752be0bc8708f37a09cb4c124/0_205_3072_1843/master/3072.jpg?width=1200&quality=85&auto=format&fit=max&s=76cf40bec019f46ed89492c8fdad0193"/>
+                    <p>@PranavJha</p>
+                    <i class="fa-brands fa-linkedin"></i>
+                </a>
+                
+            </footer>        
+        </>
     )
 }
